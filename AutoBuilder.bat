073a090a0ad6cfe0dafb06d6f613e6cd97f642d7
ConaII_ <72966048+ConaII@users.noreply.github.com>
2021-11-21 23:38:49 -0300

    Update AutoBuilder to v3.2.1

--- AutoBuilder.bat
+++ AutoBuilder.bat
@@ -1,253 +1,360 @@
 @echo off
 setlocal EnableDelayedExpansion
-SET file=spigot
-SET name=Spigot
-SET name2=Spigot and bukkit
 call :setESC
-ECHO.
-ECHO %ESC%[1m%ESC%[33mStarting Spigot-BuildTools 1.8-1.17 downloader...%ESC%[0m
-ECHO.
-goto VARS
-
-
-
-:VARS
-SET JAVA16="C:\Program Files\EclipseAdoptium\jdk-16.0.2.7-openj9\bin\java.exe"
-if exist %JAVA16% (
-	ECHO  %ESC%[92mJava 16 binary: %JAVA16%%ESC%[0m
-	ECHO.
-) else (
-	ECHO  %ESC%[31mJava 16 binary NOT FOUND: %JAVA16%%ESC%[0m
-	ECHO.
-)
-
-SET JAVA11="C:\Program Files\EclipseAdoptium\jre-11.0.13.8-hotspot\bin\java.exe"
-if exist %JAVA11% (
-	ECHO  %ESC%[92mJava 11 binary: %JAVA11%%ESC%[0m
-	ECHO.
-) else (
-	ECHO  %ESC%[31mJava 11 binary NOT FOUND: %JAVA11%%ESC%[0m
-	ECHO.
-)
-SET JAVA8="C:\Program Files\EclipseAdoptium\jre-8.0.312.7-hotspot\bin\java.exe"
-if exist %JAVA8% (
-	ECHO  %ESC%[92mJava 8 binary: %JAVA8%%ESC%[0m
-	ECHO.
-) else (
-	ECHO  %ESC%[31mJava 8 binary NOT FOUND: %JAVA8%%ESC%[0m
-	ECHO.
-)
-SET BUKKIT=FALSE
-if %BUKKIT%==TRUE ( 
-	SET CRAFTBUKKIT=--compile craftbukkit
-	SET file=bukkit
-	SET name=Bukkit
-	SET name2=Bukkit
-	)
-ECHO %ESC%[33mBukkitCompiler: %ESC%[91m%BUKKIT% %ESC%[96m(1.14+)
-ECHO.
+call :CONFIG
+call :SCRIPT
 goto START
 
-:START
 
-ECHO %ESC%[1m%ESC%[33m[SPIGOT-BUILDTOOLS 1.8-1.18 DOWNLODER]%ESC%[0m
+:CONFIG
+:: Java 16-17 or higher is required
+:: Recommended Java 16 as for now (1.17+)
+SET JDK17="C:\Program Files\EclipseAdoptium\jdk-16.0.2.7-openj9\bin\java.exe"
+:: Java 8-17 or higher are valid
+:: Recommended Java 16 (1.16.5)
+SET JDK16="C:\Program Files\EclipseAdoptium\jdk-16.0.2.7-openj9\bin\java.exe"
+:: Java 8-15 is valid
+:: Recommended Java 11 (1.13-1.16.4)
+SET JDK11="C:\Program Files\EclipseAdoptium\jre-11.0.13.8-hotspot\bin\java.exe"
+:: Java 8-10 is valid
+:: Recommended Java 8 (1.8-1.12.2)
+SET JDK8="C:\Program Files\EclipseAdoptium\jre-8.0.312.7-hotspot\bin\java.exe"
+
+SET OUTPUT="./product"
+SET CRAFTBUKKIT=TRUE
+SET SPIGOT=TRUE
+
+SET 1.17.1=TRUE
+SET 1.17=false
+::
+SET 1.16.5=TRUE
+SET 1.16.4=false
+SET 1.16.3=TRUE
+SET 1.16.2=flase
+SET 1.16.1=TRUE
+SET 1.16=false
+::
+SET 1.15.2=TRUE
+SET 1.15.1=false
+SET 1.15=false
+::
+SET 1.14.4=TRUE
+SET 1.14.3=false
+SET 1.14.2=false
+SET 1.14.1=false
+SET 1.14=false
+::
+SET 1.13.2=TRUE
+SET 1.13.1=false
+SET 1.13=false
+::
+SET 1.12.2=TRUE
+SET 1.12.1=false
+SET 1.12=false
+::
+SET 1.11.2=TRUE
+SET 1.11.1=false
+SET 1.11=false
+::
+SET 1.10.2=TRUE
+SET 1.10=false
+::
+SET 1.9.4=TRUE
+SET 1.9.2=false
+SET 1.9=false
+::
+SET 1.8.8=TRUE
+SET 1.8.7=false
+SET 1.8.6=false
+SET 1.8.5=false
+SET 1.8.4=false
+SET 1.8.3=false
+SET 1.8=false
+goto :eof
+
+:SCRIPT
 ECHO.
-ECHO %ESC%[36mBuild the org.spigot.spigot jars 1.17-1.8?%ESC%[0m
-ECHO %ESC%[91mValid: YES/NO, Yes/No, yes/no%ESC%[0m
+ECHO -----------------------------------------------
+ECHO ^| %ESC%[1m%ESC%[33mStarting %name%-BuildTools 1.8-1.17 downloader...%ESC%[0m ^|
+ECHO -----------------------------------------------
+ECHO.
+
+if exist %JDK17% (
+	ECHO  %ESC%[92mJava 17 binary: %JDK17%%ESC%[0m
+	ECHO.
+) else (
+	ECHO  %ESC%[31mJava 17 binary NOT FOUND: %JDK17%%ESC%[0m
+	ECHO.
+)
+
+if exist %JDK16% (
+	ECHO  %ESC%[92mJava 16 binary: %JDK16%%ESC%[0m
+	ECHO.
+) else (
+	ECHO  %ESC%[31mJava 16 binary NOT FOUND: %JDK16%%ESC%[0m
+	ECHO.
+)
+if exist %JDK11% (
+	ECHO  %ESC%[92mJava 11 binary: %JDK11%%ESC%[0m
+	ECHO.
+) else (
+	ECHO  %ESC%[31mJava 11 binary NOT FOUND: %JDK11%%ESC%[0m
+	ECHO.
+)
+if exist %JDK8% (
+	ECHO  %ESC%[92mJava 8 binary: %JDK8%%ESC%[0m
+	ECHO.
+) else (
+	ECHO  %ESC%[31mJava 8 binary NOT FOUND: %JDK8%%ESC%[0m
+	ECHO.
+)
+	ECHO.
+	ECHO %ESC%[33mBukkitCompiler: %ESC%[91m%CRAFTBUKKIT% %ESC%[96m(1.14+)
+	ECHO.
+	ECHO.
+
+
+
+SET packagemc=org.spigotmc.spigot
+SET packagebk=org.bukkit.craftbukkit
+if /I "%SPIGOT%"=="true" (
+	if /I "!CRAFTBUKKIT!"=="false" (
+		set package=%packagemc%
+		set file=spigot
+		set name=Spigot
+		goto :eof
+	)
+	if /I "!CRAFTBUKKIT!"=="true" (
+		set package=%packagemc% and %packagebk% ^(1.8-1.17^)
+		set file=both
+		set "name=Spigot ^& CraftBukkit"
+		goto :eof
+	)
+)
+if /I "%SPIGOT%"=="false" (
+	if /I "!CRAFTBUKKIT!"=="true" (
+		set package=%packagebk%
+		set file=craftbukkit
+		set name=CraftBukkit
+		goto :eof
+	)
+	if /I "!CRAFTBUKKIT!"=="false" (
+		set package=%packagemc% and %packagebk% ^(1.8-1.13^)
+		set file=both
+		set mode=legacy
+		set "name=Spigot ^& CraftBukkit"
+		goto :eof
+	)
+)
+goto :eof
+
+
+::
+:: Promt the user to start building.
+::
+:START
+ECHO %ESC%[1m%ESC%[33m[%name%-BUILDTOOLS 1.8-1.18 DOWNLODER]%ESC%[0m
+ECHO.
+ECHO %ESC%[36mBuild the %package% jars 1.8-1.17^?%ESC%[0m
+ECHO %ESC%[91mValid: Yes or No%ESC%[0m
 ECHO.
 set /p input="%ESC%[31m>> %ESC%[0m"
- if /i "%input%"=="" (
+ if "%input%"=="" (
+	ECHO.
 	ECHO.
 	ECHO %ESC%[91mUnknown option.%ESC%[0m
 	ECHO.
+	ECHO.
 	goto :START
  )
- if %input%==yes goto BUILD
- if %input%==no goto EXIT
- if %input%==Yes goto BUILD
- if %input%==No goto EXIT
- if %input%==YES goto BUILD
- if %input%==NO goto EXIT
-ECHO.
-ECHO %ESC%[91mUnknown option.%ESC%[0m
-ECHO.
+ if /i "%input%"=="yes" goto COMPILER
+ if /i "%input%"=="no" goto EXIT
+	ECHO.
+	ECHO.
+	ECHO %ESC%[91mUnknown option.%ESC%[0m
+	ECHO.
+	ECHO.
 goto START
 
-:BUILD
-if exist ./product/%file%-1.17.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.17 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.17 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA16% -jar BuildTools.jar --rev 1.17 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.16.5.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.16.5 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.16.5 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA16% -jar BuildTools.jar --rev 1.16.5 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.16.3.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.16.3 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.16.3 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA11% -jar BuildTools.jar --rev 1.16.3 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.16.1.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.16.1 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.16.1 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA11% -jar BuildTools.jar --rev 1.16.1 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.15.2.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.15.2 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.15.2 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA11% -jar BuildTools.jar --rev 1.15.2 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.14.4.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.14.4 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name% 1.14.4 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA11% -jar BuildTools.jar --rev 1.14.4 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.13.2.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.13.2 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.13.2 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA11% -jar BuildTools.jar --rev 1.13.2 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.12.2.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.12.2 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.12.2 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA8% -jar BuildTools.jar --rev 1.12.2 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.11.2.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.11.2 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.11.2. ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA8% -jar BuildTools.jar --rev 1.11.2 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.10.2.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.10.2 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.10.2 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA8% -jar BuildTools.jar --rev 1.10.2 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.9.4.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.9.4 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.9.4 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA8% -jar BuildTools.jar --rev 1.9.4 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
-if exist ./product/spigot-1.8.8.jar (
-	ECHO.
-	ECHO %ESC%[91m %name% 1.8.8 already built, skipping ...%ESC%[0m%ESC%[0m
-	ECHO.
-) ELSE (
-	ECHO.
-	ECHO %ESC%[92m Building %name2% 1.8.8 ...%ESC%[0m
-	ECHO %ESC%[46;30m
-	%JAVA8% -jar BuildTools.jar --rev 1.8.8 --output-dir ./product %CRAFTBUKKIT%
-	ECHO.
-)
-ECHO %ESC%[40;37m
+::
+:: Manages version compiling.
+::
+:COMPILER
+if /I "!1.17.1!"=="true" ( set version=1.17.1 & set JAVA=%JDK17% & call :DETECT )
+if /I "!1.17!"=="true" ( set version=1.17 & set JAVA=%JDK17% & call :DETECT )
+if /I "!1.16.5!"=="true" ( set version=1.16.5 & set JAVA=%JDK16% & call :DETECT )
+if /I "!1.16.4!"=="true" ( set version=1.16.4 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.16.3!"=="true" ( set version=1.16.3 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.16.2!"=="true" ( set version=1.16.2 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.16.1!"=="true" ( set version=1.16.1 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.16!"=="true" ( set version=1.16 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.15.2!"=="true" ( set version=1.15.2 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.15.1!"=="true" ( set version=1.15.1 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.15!"=="true" ( set version=1.15 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.14.4!"=="true" ( set version=1.14.4 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.14.3!"=="true" ( set version=1.14.3 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.14.2!"=="true" ( set version=1.14.2 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.14.1!"=="true" ( set version=1.14.1 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.14!"=="true" ( set version=1.14 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.13.2!"=="true" ( set version=1.13.2 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.13.1!"=="true" ( set version=1.13.1 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.13!"=="true" ( set version=1.13 & set JAVA=%JDK11% & call :DETECT )
+if /I "!1.12.2!"=="true" ( set version=1.12.2 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.12.1!"=="true" ( set version=1.12.1 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.12!"=="true" ( set version=1.12 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.11.2!"=="true" ( set version=1.11.2 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.11.1!"=="true" ( set version=1.11.1 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.11!"=="true" ( set version=1.11 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.10.2!"=="true" ( set version=1.10.2 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.10!"=="true" ( set version=1.10 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.9.4!"=="true" ( set version=1.9.4 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.9.2!"=="true" ( set version=1.9.2 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.9!"=="true" ( set version=1.9 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.9.4!"=="true" ( set version=1.9.4 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.8!"=="true" ( set version=1.8.8 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.7!"=="true" ( set version=1.8.7 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.6!"=="true" ( set version=1.8.6 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.5!"=="true" ( set version=1.8.5 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.4!"=="true" ( set version=1.8.4 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8.3!"=="true" ( set version=1.8.3 & set JAVA=%JDK8% & call :DETECT )
+if /I "!1.8!"=="true" ( set version=1.8 & set JAVA=%JDK8% & call :DETECT )
 if '%errorlevel%' == '0' ( goto FINISH ) else ( goto CRASH )
 
+::
+::
+:: Automatically detect the already built jars.
+::
+:DETECT
+if "%file%"=="both" (
+	if exist "%output%/spigot-%version%.jar" (
+		if exist "%output%/craftbukkit-%version%.jar" (
+			set final=both
+			call :skip
+			goto :eof
+		) else (
+			set final=CraftBukkit
+			set artifact=--compile craftbukkit
+			call :skip
+			call :build
+			goto :eof
+		)
+	)
+	if exist "%output%/craftbukkit-%version%.jar" (
+		if exist "%output%/spigot-%version%.jar" (
+			set final=both
+			call :skip
+			goto :eof
+		) else (
+			set final=Spigot
+			set artifact=--compile spigot
+			call :skip
+			call :build
+			goto :eof
+		)
+	) else (
+		set final=%name%
+		set artifact=--compile spigot --compile craftbukkit
+		call :build
+		goto :eof
+	)
+) else (
+	if exist "%output%/%file%-%version%.jar" (
+		set final=%name%
+		call :skip
+		goto :eof
+	) else (
+		set final=%name%
+		set artifact=--compile %file%
+		call :build
+		goto :eof
+	)
+)
+goto :eof
 
-:EXIT
-echo.
-ECHO --------------
-ECHO ^|%ESC%[1m%ESC%[91m Exiting... %ESC%[0m^|
-ECHO --------------
-TIMEOUT /T 10
-exit
+::
+:: Automatically sort the skipping build message.
+::
+:SKIP
+if "%final%"=="both" (
+	ECHO.
+	ECHO %ESC%[91m %name% %version% already built, skipping ...%ESC%[0m%ESC%[0m
+	ECHO.
+	goto :eof
+) else (
+	ECHO.
+	ECHO %ESC%[91m %final% %version% already builded, skipping ...%ESC%[0m%ESC%[0m
+	ECHO.
+	goto :eof
+)
+goto :eof
 
-:FINISH
-echo.
-ECHO ----------------------
-ECHO ^|%ESC%[1m%ESC%[92m Building finished^^! %ESC%[0m^|
-ECHO ----------------------
-echo.
-pause
-goto EXIT
+::
+:: Automatically compile the asked jar..
+::
+:BUILD
+	ECHO %ESC%[40;37m
+	ECHO.
+	ECHO %ESC%[92m Building %name% %version% ...%ESC%[0m
+	SET "RUN=%JAVA%-jar BuildTools.jar --rev %version%%artifact% --output-dir %OUTPUT%"
+	ECHO %ESC%[46;30m
+	ECHO CMD: %RUN%
+	%RUN%
+	ECHO.
+	ECHO %ESC%[40;37m
+if not '%errorlevel%' == '0' (
+	ECHO %ESC%[40;37m
+	ECHO ---------------------------------------------
+	ECHO %ESC%[1m%ESC%[31m ERROR: %ESC%[1m%ESC%[91mBUILDING %name% %version% HAS FAILED^! %ESC%[0m
+	ECHO ---------------------------------------------
+	ECHO %ESC%[40;37m
+	TIMEOUT /T 15
+)
+goto :eof
 
-:CRASH
-ECHO -------------------------------
-ECHO ^|%ESC%[1m%ESC%[31m ERROR: %ESC%[1m%ESC%[91mBUILDING HAS FAILED^^! %ESC%[0m^|
-ECHO -------------------------------
-echo.
-pause
-goto EXIT
-
+::
+:: Assign Per-line color manager.
+::
 :setESC
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
   set ESC=%%b
   exit /B 0
 )
+
+::
+:: Finished compiling.
+::
+:FINISH
+ECHO %ESC%[40;37m
+ECHO ----------------------
+ECHO ^|%ESC%[1m%ESC%[92m Building finished^! %ESC%[0m^|
+ECHO ----------------------
+ECHO %ESC%[40;37m
+echo %ESC%[91m
+pause
+goto EXIT
+
+::
+:: Crashed while compiling.
+::
+:CRASH
+ECHO %ESC%[40;37m
+ECHO ------------------------------
+ECHO ^|%ESC%[1m%ESC%[31m ERROR: %ESC%[1m%ESC%[91mBUILDING HAS FAILED^! %ESC%[0m^|
+ECHO ------------------------------
+ECHO %ESC%[40;37m
+echo %ESC%[91m
+pause
+goto EXIT
+
+::
+:: Exit the script.
+::
+:EXIT
+ECHO %ESC%[40;37m
+ECHO --------------
+ECHO ^|%ESC%[1m%ESC%[91m Exiting... %ESC%[0m^|
+ECHO --------------
+ECHO %ESC%[40;37m
+echo %ESC%[91m
+TIMEOUT /T 10
+exit
